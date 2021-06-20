600427685ad50a0982cf7fca70833bc8d0971b3a
ilhamgusti <ilham.wibowo51@gmail.com>
2021-06-20 20:35:11 +0700

    fix obj to arr

--- resources/views/emails/approval/reject.blade.php
+++ resources/views/emails/approval/reject.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Retur Barang Telah Ditolak
 
-retur barang dengan No Surat Jalan. {{$emails->no_surat_jalan}}. telah ditolak
+retur barang dengan No Surat Jalan. {{$emails['no_surat_jalan']}}. telah ditolak
 
 Thanks,<br>
 Baja Prima
